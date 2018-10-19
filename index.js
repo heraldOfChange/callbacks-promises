bf2e673c4886e1f1ff8b88ae092d4501bbb9bbc9
Martijn Hanrath <martijnjwvhanrath@gmail.com>
2018-10-19 12:58:25 +0200

    feature: solve using callbacks

--- index.js
+++ index.js
@@ -2,16 +2,19 @@
 
 let composedString = '';
 
-const composeStringWithTimeout = (str, timeout) =>
-    setTimeout(() => composedString += str, timeout);
+const composeStringWithTimeout = (str, timeout, callback) =>
+    setTimeout(() => {
+        composedString += str;
+        callback();
+    }, timeout);
 
-const composeString = () => {
-    composeStringWithTimeout('A', 400);
-    composeStringWithTimeout('B', 300);
-    composeStringWithTimeout('C', 200);
-    composeStringWithTimeout('D', 100);
-    console.info(composedString);
-};
+const composeString = () =>
+    composeStringWithTimeout('A', 400, () =>
+        composeStringWithTimeout('B', 300, () =>
+            composeStringWithTimeout('C', 200, () =>
+                composeStringWithTimeout('D', 100, () => console.info(composedString))
+            )
+        )
+    );
 
 setTimeout(composeString, 1000);
-composeString();
